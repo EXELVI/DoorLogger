726b084295faf1bfff95a02562640c6adc93cbeb
EXELVI <exelvi@outlook.com>
2024-07-20 22:36:03 +0200

    Update Doorlogger.ino

--- Doorlogger.ino
+++ Doorlogger.ino
@@ -1,15 +1,21 @@
 #include "WiFiS3.h"
 #include "WiFiSSLClient.h"
 #include "arduino_secrets.h"
+#include <WiFiUdp.h>
 #include <Arduino_JSON.h>
+#include "RTC.h"
+#include <Wire.h>
+#include <NTPClient.h>
 
 #define magneticSwitch 2
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
-int keyIndex = 0;  // your network key Index number (needed only for WEP)
+int keyIndex = 0; // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
+WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+NTPClient timeClient(Udp);
 
 char server[] = "discord.com";
 char path[] = SECRET_webhook;
@@ -17,11 +23,16 @@ WiFiSSLClient client;
 
 bool lastState = false;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(magneticSwitch, INPUT_PULLUP);
+
+  RTC.begin();
+
   // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
+  while (status != WL_CONNECTED)
+  {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network:
@@ -31,21 +42,33 @@ void setup() {
   }
   Serial.println("Connected to wifi");
   printWifiStatus();
+
+  timeClient.begin();
+  timeClient.update();
+
 }
 
-void loop() {
-  if (client.available()) {
+void loop()
+{
+  if (client.available())
+  {
     Serial.println("client available");
     String line = client.readStringUntil('\r');
     Serial.println(line);
-  } else {
+  }
+  else
+  {
     int sensorValue = digitalRead(magneticSwitch);
 
-    if (sensorValue != lastState) {
-      if (sensorValue == HIGH) {
+    if (sensorValue != lastState)
+    {
+      if (sensorValue == HIGH)
+      {
         Serial.println("Door opened");
         sendWebhook(sensorValue);
-      } else {
+      }
+      else
+      {
         Serial.println("Door closed");
         sendWebhook(sensorValue);
       }
@@ -56,12 +79,21 @@ void loop() {
   }
 }
 
-void sendWebhook(int sensorValue) {
+String discordTimestamp(int timestamp, String format)
+{
+
+}
+
+void sendWebhook(int sensorValue)
+{
+  RTCTime currentTime;
+  RTC.getTime(currentTime);
+
   String content = sensorValue ? "The door was opened!" : "The door was closed!";
   JSONVar body;
   body["content"] = content;
   body["embeds"][0]["title"] = "The door was " + String(sensorValue ? "opened" : "closed") + "!";
-  body["embeds"][0]["description"] = "The door was " + String(sensorValue ? "opened" : "closed") + "!";
+  body["embeds"][0]["description"] = "The door was " + String(sensorValue ? "opened" : "closed") + "at " + discordTimestamp(currentTime.getEpochTime(), "FULL");
   body["embeds"][0]["color"] = sensorValue ? 3931904 : 16711680;
 
   String bodyString = JSON.stringify(body);
@@ -72,7 +104,8 @@ void sendWebhook(int sensorValue) {
   Serial.print("Connecting to server: ");
   Serial.println(server);
 
-  if (client.connect(server, 443)) {
+  if (client.connect(server, 443))
+  {
     Serial.println("connected to server");
     client.println("POST " + String(path) + " HTTP/1.1");
     client.println("Host: " + String(server));
@@ -82,20 +115,25 @@ void sendWebhook(int sensorValue) {
     client.println();
     client.println(bodyString);
 
-    while (client.connected()) {
+    while (client.connected())
+    {
       String response = client.readStringUntil('\n');
-      if (response.length() == 0) {
+      if (response.length() == 0)
+      {
         break;
       }
       Serial.println(response);
     }
     client.stop();
-  } else {
+  }
+  else
+  {
     Serial.println("connection failed");
   }
 }
 
-void printWifiStatus() {
+void printWifiStatus()
+{
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
